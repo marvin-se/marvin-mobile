fcca0cb7b3c79b190920b35d98038bec913bae84
harundmrpnce <harundemirpence@gmail.com>
2025-12-13 16:12:22 +0300

    add util functions

---
+++ utils/constants.ts
@@ -0,0 +1,18 @@
+export const UNIVERSITIES = [
+    "Harvard University",
+    "Stanford University",
+    "MIT",
+    "UCLA",
+    "Princeton University",
+    "Yale University",
+].map((u) => ({ label: u, value: u }));
+
+export const COLORS = {
+    primary: "#72c69b",
+    secondary: "#182c53",
+    text: "#5a6778",
+    background: "#fdf6e3",
+    border: "#d1d5db",
+    white: "#ffffff",
+    error: "#ef4444",
+};
